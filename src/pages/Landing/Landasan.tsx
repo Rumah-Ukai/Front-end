2c69ff393cca02eab2c2c078f7c3e24d6630d3ff
SpexID <ngurahadipermana15@gmail.com>
2025-11-27 10:56:26 +0800

    foto terakhir

--- src/pages/Landing/Landasan.tsx
+++ src/pages/Landing/Landasan.tsx
@@ -2,7 +2,7 @@
 import { Stack, Typography, Button, Box, Card, CardContent } from '@mui/material';
 
 
-import heroImg from '../../assets/tryout ukmppai.svg';
+import heroImg from '../../assets/tryout.svg';
 import { useEffect } from 'react';
 
 
