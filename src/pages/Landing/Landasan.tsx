9feb71df4b88985b1f9d09a18632896bd0e8393b
SpexID <ngurahadipermana15@gmail.com>
2025-11-27 11:07:20 +0800

    po

--- src/pages/Landing/Landasan.tsx
+++ src/pages/Landing/Landasan.tsx
@@ -2,7 +2,7 @@
 import { Stack, Typography, Button, Box, Card, CardContent } from '@mui/material';
 
 
-import heroImg from '../../assets/tryout.svg';
+import heroImg from '../../assets/tryout.png';
 import { useEffect } from 'react';
 
 
